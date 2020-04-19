8b6812004de67a644003b65e13e3b3aa42d99b18
Turar Arykbaev <turar.arykbaev@gmail.com>
2020-04-20 00:47:47 +0600

    Updates

---
+++ precache-manifest.9c605ea6d346c38002e76566d4be76a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a4d460c96e71a500ac094714ad4431e",
+    "url": "/make-memories/index.html"
+  },
+  {
+    "revision": "5b61b3f811aae2e674dc",
+    "url": "/make-memories/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "09cd520973a82ded6d54",
+    "url": "/make-memories/static/js/2.fcdb3b9e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/make-memories/static/js/2.fcdb3b9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b61b3f811aae2e674dc",
+    "url": "/make-memories/static/js/main.2371c025.chunk.js"
+  },
+  {
+    "revision": "623550cae16eb51c9e91",
+    "url": "/make-memories/static/js/runtime-main.a5e3848e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/make-memories/static/media/logo.5d5d9eef.svg"
+  }
+]);
